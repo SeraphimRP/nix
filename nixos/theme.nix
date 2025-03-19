4b2d5a48beaa1b5221a04abaf312af4b397661d0
Seraphim R. Pardee <me@srp.life>
2025-03-18 22:24:57 -0400

    moar themes

--- nixos/theme.nix
+++ nixos/theme.nix
@@ -14,11 +14,14 @@
     
     # lots to do here, compare to https://github.com/XNM1/linux-nixos-hyprland-config-dotfiles/blob/main/nixos/theme.nix
     environment.systemPackages = with pkgs; [
+        papirus-nord
+        nordzy-icon-theme
+        nordzy-cursor-theme
         nordic
         inputs.mcmojave-hyprcursor.packages.${pkgs.stdenv.hostPlatform.system}.default
         glib
         gsettings-desktop-schemas
-        nwg-look
+        nwg-look # we do all the configuration in here
         kitty-themes
     ];
 }
