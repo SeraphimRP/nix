b85fd9dae9a252eb1ccb42c1d3d465cb6156dd5e
Seraphim R. Pardee <me@srp.life>
2025-07-16 22:09:57 -0400

    Clean gc after install.

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 dconf write /org/gtk/settings/file-chooser/clock-format "'12h'"
-sudo cp -R nixos/* /etc/nixos/
+#sudo cp -R nixos/* /etc/nixos/
 sudo cp profile.local /etc/profile.local
 #nix flake update --flake /etc/nixos 
 #sudo nixos-rebuild switch --flake /etc/nixos#nixos --commit-lock-file --upgrade --show-trace 2>/dev/stdout
-cd nixos
-nh os switch . -H nixos -u
-nh home switch . -u
-cd ..
+nh os switch ./nixos -H nixos -u
+nh home switch ./nixos -u
+nh clean all --keep-since 4d --keep 3
